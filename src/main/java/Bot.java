b6d63fc740b0a978afa57cc2efc7075d390696f0
sermeds <Mr_sermed_000@mail.ru>
2022-12-03 01:57:07 +0400

    initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,57 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "LazyBulbasaurusBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5823957267:AAHiMkAcrkQ_PWd0OTDLDxkISkEcbMIpYBY";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case ("/help"):
+                    sendMsg(message, "Чем могу помочь?");
+                    break;
+                case ("/start"):
+                    sendMsg(message, "Привествую");
+                    break;
+                default:
+
+            }
+        }
+    }
+
+    public void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Update.java
@@ -0,0 +1,64 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Update {
+
+    public static String timeUp = "0";
+
+    public static void timer() throws IOException {
+        Calendar calendarTimer = new GregorianCalendar();
+        if (calendarTimer.get(Calendar.HOUR_OF_DAY) == 9 && calendarTimer.get(Calendar.MINUTE) == 0 && calendarTimer.get(Calendar.SECOND) == 0) {
+            parseHtml();
+        }
+    }
+
+    public static void parseHtml() throws IOException {
+        deleteOld();
+        Document document = Jsoup.connect("https://rasp.sstu.ru/").get();
+        Elements elements = document.select("a");
+        for (Element element : elements) {
+            System.out.println(element.attr("abs:href"));
+            downloadFile(element.attr("abs:href"), Paths.get("D:\\!TGBOT"));
+        }
+        timeUp = new Date().toString();
+    }
+
+    public static void deleteOld() {
+        File file = new File("D:\\!TGBOT");
+        if (file.length() != 0) {
+            for (File file1 : file.listFiles()) {
+                file1.delete();
+            }
+        }
+    }
+
+    public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
+        URL url = new URL(urlString);
+        InputStream inputStream = url.openStream();
+
+        Path tmp = Files.createTempFile("temp-", ".tmp");
+        Files.copy(inputStream, tmp, StandardCopyOption.REPLACE_EXISTING);
+
+        String fieName = urlString.substring(urlString.lastIndexOf("/"));
+        Path destPath = Paths.get(downloadDirectory.toString(), fieName);
+        Files.move(tmp, destPath);
+
+        return destPath;
+    }
+
+}
+
