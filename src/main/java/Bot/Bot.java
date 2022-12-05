5f3c5ae4b098596b6e254d2f1497fb4b38d7deb0
LordOfSkai <LordOfSkai@mail.ru>
2022-12-06 01:20:15 +0400

    Добавлены рабочие факты,советы и цитаты! Добавил полностью рабочие заметки заметки! Рабочий вызов рандома из советов, цитат и фактов! Готова система обратной связи и /pm по id!

--- src/main/java/Bot/Bot.java
+++ src/main/java/Bot/Bot.java
@@ -9,6 +9,8 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -83,6 +85,8 @@ public class Bot extends TelegramLongPollingBot {
                 }
                 break;
             case ("мем"):
+//                sendImage("https://fikiwiki.com/uploads/posts/2022-02/1644965605_9-fikiwiki-com-p-kartinki-priroda-na-zastavku-telefona-9.jpg" ,message.getChatId().toString());
+//                sendImage("src/main/resources/test4.jpeg",message.getChatId().toString());
                 sendImage(message);
                 break;
             case ("стипендия"):
@@ -95,12 +99,42 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
+//    public void sendImage(String url, String chatId) {
+//        // Create send method
+//        SendPhoto sendPhotoRequest = new SendPhoto();
+//        // Set destination chat id
+//        sendPhotoRequest.setChatId(chatId);
+//        // Set the photo url as a simple photo
+//        sendPhotoRequest.setPhoto(new InputFile(url));
+//        try {
+//            // Execute the method
+//            execute(sendPhotoRequest);
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    public void sendImage(String filePath, String chatId) {
+        // Create send method
+        SendPhoto sendPhotoRequest = new SendPhoto();
+        // Set destination chat id
+        sendPhotoRequest.setChatId(chatId);
+        // Set the photo file as a new photo (You can also use InputStream with a constructor overload)
+        sendPhotoRequest.setPhoto(new InputFile(new File(filePath)));
+        try {
+            // Execute the method
+            execute(sendPhotoRequest);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void sendImage(Message message){
         SendPhoto sendPhoto = new SendPhoto();
         sendPhoto.setChatId(message.getChatId().toString());
         try {
-//            sendPhoto.setPhoto(ImgParser.imageParser());
-            sendPhoto.setPhoto(new InputFile("src/main/resources/test4.jpeg"));
+            sendPhoto.setPhoto(ImgParser.imageParser());
+//            sendPhoto.setPhoto(new InputFile(("src/main/resources/test4.jpeg")));
             execute(sendPhoto);
         } catch (TelegramApiException e) {
             e.printStackTrace();
--- src/main/java/Bot/ImgParser.java
+++ src/main/java/Bot/ImgParser.java
@@ -23,7 +23,7 @@ public class ImgParser {
             File file1 = new File("src/main/resources/test3.jpeg");
             try (InputStream in = new URL(img.first().attr("src")).openStream()) {
                 FileUtils.copyURLToFile(new URL(img.first().attr("src")), file1);
-                return new InputFile("src/main/resources/test3.jpeg");
+                return new InputFile(file1);
             } catch (IOException e) {
                 System.out.println("Сукаааа, опять тупой копи не сработал");
                 e.printStackTrace();
